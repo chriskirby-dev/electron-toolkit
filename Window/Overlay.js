e74cb948b24cc0fb197bd7de7ec13322d3339b20
Chris Kirby <chris@chriskirby.me>
2024-11-25 13:35:51 -0500

    Added Overlay

--- Window/Overlay.js
+++ Window/Overlay.js
@@ -1,11 +1,32 @@
 import FluxBrowserWindow from "../Base/FluxBrowserWindow.js";
-
+import { Window } from "../vendor/windows-koffi";
 class Overlay extends FluxBrowserWindow {
-    constructor(options) {
+    constructor(target, options) {
+        options.alwaysOnTop = true;
+        (options.transparent = true), (options.frame = false);
+        if (!options.webPreferences) options.webPreferences = {};
+        options.webPreferences.preload = "../Renderer/preload.js";
         super(options);
+        this._target = target;
     }
 
     initialize() {
-        const { config, options } = this;
+        const { config, options, _target } = this;
+
+        if (_target) {
+            this.target = Window.from(_target);
+            this.target.onChange((e) => {
+                if (e.isMoved) {
+                    this.x = win.client.x;
+                    this.y = win.client.y;
+                }
+
+                if (e.isResized) {
+                    this.width = win.rect.width;
+                    this.height = win.rect.height;
+                }
+                this.setAlwaysOnTop(true, "screen");
+            });
+        }
     }
 }
