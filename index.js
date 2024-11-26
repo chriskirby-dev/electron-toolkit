0573d91f8249d92110cddfef851268794d6c0604
Chris Kirby <chris@chriskirby.me>
2024-11-25 19:16:35 -0500

    Added index

---
+++ index.js
@@ -0,0 +1,9 @@
+import BrowserWindow from "./Base/FluxBrowserWindow.js";
+import OverlayWindow from "./Window/Overlay.js";
+
+export default {
+    BrowserWindow,
+    OverlayWindow,
+};
+
+export { BrowserWindow, OverlayWindow };
